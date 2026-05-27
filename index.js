2f5b579c82e53ab670f08d0a54aacf49553dc5e8
Olina Chakraborty <olinach12@gmail.com>
2026-05-27 21:08:23 +0530

    Fix render port issue

--- index.js
+++ index.js
@@ -331,6 +331,8 @@ app.get("/dashboard", async (req, res) => {
   }
 });
 
-app.listen(5000,()=>{
-    console.log("Server running")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
